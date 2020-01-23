2333bac43a08e52b5f30646427867c0ab1b2e67b
Jason Miller <developit@google.com>
2020-01-23 13:09:07 -0500

    add markdown tables

--- action.js
+++ action.js
@@ -2,18 +2,16 @@ import { getInput, setFailed } from '@actions/core';
 import { GitHub, context } from '@actions/github';
 import { exec } from '@actions/exec';
 import SizePlugin from 'size-plugin-core';
+import prettyBytes from 'pretty-bytes';
 
 
 async function run(octokit, context) {
 	const { owner, repo, number: pull_number } = context.issue;
-	console.log('context', { owner, repo, pull_number });
 
 	const pr = (await octokit.pulls.get({ owner, repo, pull_number })).data;
 
-	console.log('pr', pr);
-
 	const plugin = new SizePlugin({
-		pattern: process.env.PATTERN || 'dist/*.js,**/dist/*.js',
+		pattern: process.env.PATTERN || '**/dist/*.js',
 		exclude: '*.map'
 	});
 
@@ -23,13 +21,8 @@ async function run(octokit, context) {
 	await exec(`npm ci && npm run build`);
 	const newSizes = await plugin.readFromDisk(cwd);
 
-	console.log('GIT INFO');
 	try {
-		await exec(`git remotes -v`);
-		await exec(`git branch -av`);
-	} catch (e) {}
-	try {
-		await exec(`git fetch`);
+		await exec(`git fetch -n ${pr.base.sha}`);
 	} catch (e) {
 		console.log('fetch failed', e.message);
 	}
@@ -41,13 +34,64 @@ async function run(octokit, context) {
 
 	const diff = await plugin.getDiff(oldSizes, newSizes);
 
-	const text = await plugin.printSizes(diff);
+	const cliText = await plugin.printSizes(diff);
+	console.log('SIZE DIFFERENCES:\n\n' + cliText);
+
+	const markdownDiff = diffTable(diff);
 
 	await octokit.issues.createComment({
 		...context.repo,
 		issue_number: pull_number,
-		body: `Size updates:\n\n\n\n\`\`\`\n${text}\n\`\`\``,
+		body: markdownDiff
 	});
+}
+
+function diffTable(files, showTotal) {
+	let out = `| Filename | Size | Difference | |\n`;
+	out += `| ---:|:---:|:---:|:---:|\n`;
+	let totalSize = 0;
+	let totalDelta = 0;
+	for (const file of files) {
+		const { filename, size, delta } = file;
+		totalSize += size;
+		totalDelta += delta;
+		
+		const difference = (delta / size * 100) | 0;
+		let deltaText = getDeltaText(delta, difference);
+		let icon = iconForDifference(difference);
+		out += `| \`${filename}\` | ${prettyBytes(size)} | ${deltaText} | ${icon} |\n`;
+	}
+
+	if (showTotal) {
+		const totalDifference = (totalDelta / totalSize * 100) | 0;
+		let totalDeltaText = getDeltaText(totalDelta, totalDifference);
+		let totalIcon = iconForDifference(totalDifference);
+		out = `**Total Size:** ${prettyBytes(totalSize)}\n\n`;
+		out = `**Size Change:** ${totalDeltaText} ${totalIcon}\n\n`;
+	}
+
+	return out;
+}
+
+function getDeltaText(delta, difference) {
+	let deltaText = (delta > 0 ? '+' : '') + prettyBytes(delta);
+	if (delta && Math.abs(delta) > 1) {
+		deltaText += ` (${Math.abs(difference)}%)`;
+	}
+	return deltaText;
+}
+
+function iconForDifference(difference) {
+	let icon = '';
+	if (difference >= 5) icon = '🔍';
+	else if (difference >= 10) icon = '⚠️';
+	else if (difference >= 20) icon = '🚨';
+	else if (difference >= 50) icon = '🆘';
+	else if (difference <= -5) icon = '✅';
+	else if (difference <= -10) icon = '👏';
+	else if (difference <= 20) icon = '🎉';
+	else if (difference <= 50) icon = '🏆';
+	return icon;
 }
 
 (async () => {
